86d64fcca6d6443503ea6274110ffe54325b8599
skytomo <tomo1kawa.fullerene@gmail.com>
2020-12-30 21:24:01 +0900

    Add super_words.json

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,26 @@ pub struct SuperLanguages {
     languages: HashMap<String, f64>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct SuperLanguage {
-    language: String,
-    weight: f64,
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SuperWords {
+    words: Vec<SuperWord>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SuperWord {
+    id: usize,
+    meaning: String,
+    loans: HashMap<String, String>,
+    origins: HashMap<String, String>,
 }
 
 fn main() {
-    let file = File::open("data/language_weight.json").unwrap();
-    let reader = BufReader::new(file);
-    let super_languages: SuperLanguages = serde_json::from_reader(reader).unwrap();
+    let file1 = File::open("data/language_weight.json").unwrap();
+    let file2 = File::open("data/super_words.json").unwrap();
+    let reader1 = BufReader::new(file1);
+    let reader2 = BufReader::new(file2);
+    let super_languages: SuperLanguages = serde_json::from_reader(reader1).unwrap();
+    let super_words: SuperWords = serde_json::from_reader(reader2).unwrap();
     println!("{:?}", super_languages);
+    println!("{:?}", super_words);
 }
