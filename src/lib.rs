e90a4b57067cbdf27400dd750837b8080c849018
skytomo <tomo1kawa.fullerene@gmail.com>
2020-12-31 23:25:45 +0900

    Add CharExt and StringExt

---
+++ src/lib.rs
@@ -0,0 +1,101 @@
+pub mod main;
+
+#[cfg(test)]
+mod tests {
+    use crate::main::*;
+
+    #[test]
+    fn test_w202_and_w203() {
+        assert!("pa".to_string().is_match_w202_and_w203());
+        assert!("ap".to_string().is_match_w202_and_w203());
+        assert!(!"aa".to_string().is_match_w202_and_w203());
+        assert!(!"pp".to_string().is_match_w202_and_w203());
+        assert!("a".to_string().is_match_w202_and_w203());
+        assert!("p".to_string().is_match_w202_and_w203());
+        assert!("".to_string().is_match_w202_and_w203());
+    }
+
+    #[test]
+    fn test_w204() {
+        assert!("kt".to_string().is_match_w204());
+        assert!("ap".to_string().is_match_w204());
+        assert!("pa".to_string().is_match_w204());
+        assert!(!"bp".to_string().is_match_w204());
+        assert!(!"pb".to_string().is_match_w204());
+        assert!("k".to_string().is_match_w204());
+        assert!("t".to_string().is_match_w204());
+        assert!("".to_string().is_match_w204());
+    }
+
+    #[test]
+    fn test_w205() {
+        assert!("xa".to_string().is_match_w205());
+        assert!("ax".to_string().is_match_w205());
+        assert!(!"xp".to_string().is_match_w205());
+        assert!(!"hp".to_string().is_match_w205());
+        assert!(!"yp".to_string().is_match_w205());
+        assert!(!"wp".to_string().is_match_w205());
+        assert!(!"px".to_string().is_match_w205());
+        assert!(!"ph".to_string().is_match_w205());
+        assert!(!"py".to_string().is_match_w205());
+        assert!(!"pw".to_string().is_match_w205());
+        assert!("x".to_string().is_match_w205());
+        assert!("p".to_string().is_match_w205());
+        assert!("".to_string().is_match_w205());
+    }
+
+    #[test]
+    fn test_w206() {
+        assert!("tsa".to_string().is_match_w206());
+        assert!("tca".to_string().is_match_w206());
+        assert!("dza".to_string().is_match_w206());
+        assert!("dja".to_string().is_match_w206());
+        assert!("ats".to_string().is_match_w206());
+        assert!("atc".to_string().is_match_w206());
+        assert!("adz".to_string().is_match_w206());
+        assert!("adj".to_string().is_match_w206());
+        assert!("apa".to_string().is_match_w206());
+        assert!("apk".to_string().is_match_w206());
+        assert!(!"pkt".to_string().is_match_w206());
+        assert!("pk".to_string().is_match_w206());
+        assert!("p".to_string().is_match_w206());
+        assert!("".to_string().is_match_w206());
+    }
+
+    #[test]
+    fn test_w207() {
+        assert!("aa".to_string().is_match_w207());
+        assert!("pp".to_string().is_match_w207());
+        assert!(!"cj".to_string().is_match_w207());
+        assert!("a".to_string().is_match_w207());
+        assert!("c".to_string().is_match_w207());
+        assert!("".to_string().is_match_w207());
+    }
+
+    #[test]
+    fn test_w208() {
+        assert!("t".to_string().is_match_w208());
+        assert!("ta".to_string().is_match_w208());
+        assert!(!"a".to_string().is_match_w208());
+        assert!("".to_string().is_match_w208());
+    }
+
+    #[test]
+    fn test_w209() {
+        assert!("t".to_string().is_match_w209());
+        assert!("at".to_string().is_match_w209());
+        assert!(!"a".to_string().is_match_w209());
+        assert!("".to_string().is_match_w209());
+    }
+
+    #[test]
+    fn test_w210() {
+        assert!("ts".to_string().is_match_w210());
+        assert!("tc".to_string().is_match_w210());
+        assert!("dz".to_string().is_match_w210());
+        assert!("dj".to_string().is_match_w210());
+        assert!(!"kt".to_string().is_match_w210());
+        assert!("aa".to_string().is_match_w210());
+        assert!("".to_string().is_match_w210());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,165 @@ pub struct SuperWord {
     origins: HashMap<String, String>,
 }
 
+trait CharExt {
+    fn is_vowel(&self) -> bool;
+    fn is_consonant(&self) -> bool;
+    fn is_voiced(&self) -> bool;
+    fn is_unvoiced(&self) -> bool;
+    fn is_xhyw(&self) -> bool;
+    fn is_cjsz(&self) -> bool;
+}
+
+impl CharExt for char {
+    fn is_vowel(&self) -> bool {
+        *self == 'i' || *self == 'e' || *self == 'a' || *self == 'o' || *self == 'u'
+    }
+
+    fn is_consonant(&self) -> bool {
+        *self == 'p'
+            || *self == 'b'
+            || *self == 't'
+            || *self == 'd'
+            || *self == 'k'
+            || *self == 'g'
+            || *self == 'm'
+            || *self == 'n'
+            || *self == 'r'
+            || *self == 'f'
+            || *self == 'v'
+            || *self == 's'
+            || *self == 'z'
+            || *self == 'c'
+            || *self == 'j'
+            || *self == 'x'
+            || *self == 'h'
+            || *self == 'y'
+            || *self == 'l'
+            || *self == 'w'
+    }
+
+    fn is_voiced(&self) -> bool {
+        *self == 'b' || *self == 'd' || *self == 'g' || *self == 'v' || *self == 'z' || *self == 'j'
+    }
+
+    fn is_unvoiced(&self) -> bool {
+        *self == 'p' || *self == 't' || *self == 'k' || *self == 'f' || *self == 's' || *self == 'c'
+    }
+
+    fn is_xhyw(&self) -> bool {
+        *self == 'x' || *self == 'h' || *self == 'y' || *self == 'w'
+    }
+
+    fn is_cjsz(&self) -> bool {
+        *self == 'c' || *self == 'j' || *self == 's' || *self == 'z'
+    }
+}
+
+pub trait StringExt {
+    fn nth(&self, n: usize) -> Option<char>;
+    fn rev_nth(&self, n: usize) -> Option<char>;
+    fn is_match_w202_and_w203(&self) -> bool;
+    fn is_match_w204(&self) -> bool;
+    fn is_match_w205(&self) -> bool;
+    fn is_match_w206(&self) -> bool;
+    fn is_match_w207(&self) -> bool;
+    fn is_match_w208(&self) -> bool;
+    fn is_match_w209(&self) -> bool;
+    fn is_match_w210(&self) -> bool;
+}
+
+impl StringExt for String {
+    fn nth(&self, n: usize) -> Option<char> {
+        self.chars().nth(n)
+    }
+
+    fn rev_nth(&self, n: usize) -> Option<char> {
+        self.chars().rev().nth(n)
+    }
+
+    fn is_match_w202_and_w203(&self) -> bool {
+        match (self.nth(0), self.nth(1)) {
+            (Some(a), Some(b)) => a != b,
+            _ => true,
+        }
+    }
+
+    fn is_match_w204(&self) -> bool {
+        match (self.nth(0), self.nth(1)) {
+            (Some(a), Some(b)) => {
+                !(a.is_voiced() && b.is_unvoiced() || a.is_unvoiced() && b.is_voiced())
+            }
+            _ => true,
+        }
+    }
+
+    fn is_match_w205(&self) -> bool {
+        match (self.nth(0), self.nth(1)) {
+            (Some(a), Some(b)) => {
+                !(a.is_xhyw() && b.is_consonant() || a.is_consonant() && b.is_xhyw())
+            }
+            _ => true,
+        }
+    }
+
+    fn is_match_w206(&self) -> bool {
+        match (self.nth(0), self.nth(1), self.nth(2)) {
+            (Some('t'), Some('s'), Some(_)) => true,
+            (Some('t'), Some('c'), Some(_)) => true,
+            (Some('d'), Some('z'), Some(_)) => true,
+            (Some('d'), Some('j'), Some(_)) => true,
+            (Some(_), Some('t'), Some('s')) => true,
+            (Some(_), Some('t'), Some('c')) => true,
+            (Some(_), Some('d'), Some('z')) => true,
+            (Some(_), Some('d'), Some('j')) => true,
+            (Some(a), Some(b), Some(c)) => {
+                !(a.is_consonant() && b.is_consonant() && c.is_consonant())
+            }
+            _ => true,
+        }
+    }
+
+    fn is_match_w207(&self) -> bool {
+        match (self.nth(0), self.nth(1)) {
+            (Some(a), Some(b)) => !(a.is_cjsz() && b.is_cjsz()),
+            _ => true,
+        }
+    }
+
+    fn is_match_w208(&self) -> bool {
+        match self.nth(0) {
+            Some(a) => a.is_consonant(),
+            _ => true,
+        }
+    }
+
+    fn is_match_w209(&self) -> bool {
+        match self.rev_nth(0) {
+            Some(a) => a.is_consonant(),
+            _ => true,
+        }
+    }
+
+    fn is_match_w210(&self) -> bool {
+        match (self.nth(0), self.nth(1)) {
+            (Some('t'), Some('s')) => true,
+            (Some('t'), Some('c')) => true,
+            (Some('t'), Some(_)) => false,
+            (Some('d'), Some('z')) => true,
+            (Some('d'), Some('j')) => true,
+            (Some('d'), Some(_)) => false,
+            (Some(a), Some(b)) => !(a.is_consonant() && b.is_consonant()),
+            _ => true,
+        }
+    }
+}
+
+pub struct CandidateWord {
+    pub word: String,
+    pub score: f64,
+}
+
+#[warn(dead_code)]
 fn main() {
     let file1 = File::open("data/language_weight.json").unwrap();
     let file2 = File::open("data/super_words.json").unwrap();
